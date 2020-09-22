bc61f323a61eec6e8c6ac5d66430cee33de39055
KateLockhart <katelockhart707@gmail.com>
2020-09-21 21:33:56 -0400

    Add project files.

---
+++ RestaurantRaterRedBadge/Controllers/RestaurantController.cs
@@ -0,0 +1,22 @@
+﻿using RestaurantRaterRedBadge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRaterRedBadge.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        //create a field to access database
+        private RestaurantDbContext _db = new RestaurantDbContext();
+
+        // GET: Restaurant/Index
+        public ActionResult Index()
+        {
+            //add the restaurants from the data base to the view through a list
+            return View(_db.Restaurants.ToList());
+        }
+    }
+}
